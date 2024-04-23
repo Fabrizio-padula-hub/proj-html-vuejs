b5b724e3c8e0449705816c1f21e57f86d5c0eb61
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-04-23 11:29:24 +0200

    feat: header revaluation and hero addition

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -25,12 +25,31 @@ export default{
                 <!-- /menu nav-bar -->
             </div>
         </section>
+        <!-- info-sections -->
+        <div class="info-hero">
+            <span>FRESH & TASTY BAKERY EVERY DAY</span>
+            <h2>
+                The Perfect Fresh Bread
+            </h2>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quia tenetur autem alias accusantium laboriosam ut exerci
+            </p>
+            <button class="btn">Explore our products</button>
+
+        </div>
+        <!-- info-sections -->
     </header>
 
 </template>
 
 <style scoped lang="scss">
 @use '../style/partials/variables' as*;
+header{
+    min-height: 500px;
+    background-image: url(../assets/images/hero-header-bakery.jpg);
+    background-position: 10% 50%;
+    background-size: cover;
+}
 
 .nav-bar{
     padding: 15px;
@@ -56,8 +75,32 @@ export default{
             }
         }
     }
+}
+.info-hero{
+    padding: 30px;
+    width: 35%;
+    margin: 30px 20px;
 
-    
+    span{
+        font-family: "Poppins", sans-serif;
+        font-size: 9px;
+        color: #958e9b;
+    }
+    h2{
+        color: $color-primary;
+        font-size: 30px;
+        font-weight: 800;
+        margin: 16px 0;
+    }
+    p{
+        font-family: "Poppins", sans-serif;
+        font-size: 9px;
+        color: #958e9b;
+        margin-bottom: 16px;
+    }
+    button{
+        color: $color-primary;
+    }
 }
 
 
